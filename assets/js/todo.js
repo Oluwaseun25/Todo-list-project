77bbca9be4512f88a41b8eb0a17ca08218b549c9
Oluwaseun25 <Samuelawe842@gmail.com>
2023-06-24 10:39:21 +0000

    todojs

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,28 +1,40 @@
-// nb+\=-&_ 
-
-$("ul").on("click","li",function(){
-    $(this).toggleClass("listDesign");
-});
-
-$("ul").on("click","span",function(event){ 
-    // $(this).parent().remove(); use fade instead of remove
-    $(this).parent().fadeOut(400, function(){
-        $(this).remove();
+$(document).ready(function() {
+    // Event handler for toggling class on li elements
+    $("ul").on("click", "li", function() {
+      $(this).toggleClass("listDesign");
     });
-    event.stopPropagation();
-    // event propagation is used to stop event bubblin
-});
-
-$("input[type = 'text'").on("keypress", function(event){
-    if(event.which === 13){
-        // get new todo text from input
-        var todoText = $(this).val();
-        // remove value from input after pressin enter
-        $(this).val("");
-        $("ul").append("<li><span><i class='fa fa-trash'></i></span> "  + todoText + " </li>" )
-    } 
-});
-
-$("h1 > i").on("click",function(){
-    $("input[type = 'text'").fadeToggle(400);
-})
+  
+    // Event handler for removing li elements
+    $("ul").on("click", "span", function(event) {
+      $(this)
+        .parent()
+        .fadeOut(400, function() {
+          $(this).remove();
+        });
+      event.stopPropagation();
+    });
+  
+    // Event handler for adding new todo items
+    $("input[type='text']").on("keypress", function(event) {
+      if (event.which === 13) {
+        var todoText = $(this).val().trim();
+  
+        if (todoText) {
+          $(this).val("");
+  
+          var newTodo = $("<li>").html(`
+            <span><i class="fa fa-trash"></i></span>
+            ${todoText}
+          `);
+  
+          $("ul").append(newTodo);
+        }
+      }
+    });
+  
+    // Event handler for toggling input visibility
+    $("h1 > i").on("click", function() {
+      $("input[type='text']").fadeToggle(400);
+    });
+  });
+  
